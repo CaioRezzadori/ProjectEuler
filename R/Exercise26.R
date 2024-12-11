c5a47d4b3ee2b81614fbbd8fefd81af02824ad6b
caio <caio.rezzadori@gmail.com>
2024-12-11 19:51:31 -0300

    Solving Exercise 26

--- R/Exercise26.R
+++ R/Exercise26.R
@@ -1,68 +1,31 @@
-denominatorAdjust <- function(denom){
-    while(denom %% 2 == 0){
-        denom <- denom / 2
-    }
-    while(denom %% 5 == 0){
-        denom <- denom / 5
-    }
-    return(denom)
-}
-
-decimalDivision <- function(num, denom, precision){
+decimalDivision <- function(num, denom){
     remainder <- num %% denom
-    digits <- integer(precision)
-    index <- 0
-    while(index < precision){
-        index <- index + 1
-        remainder <- remainder*10
-        digits[index] <- remainder %/% denom
+    quotients <- c(remainder)
+    remainder <- remainder * 10
+    while(!(remainder %% denom) %in% quotients){
         remainder <- remainder %% denom
+        quotients <- c(quotients, remainder)
+        remainder <- remainder * 10
     }
-    return(digits)
+    quotients <- c(quotients, remainder %% denom)
+    return(quotients)
 }
 
-
-getCycleFraction <- function(num = 1, denom){
-    denom <- denominatorAdjust(denom)
-
-    remainder <- num %% denom
-    cycle <- c((remainder * 10) %/% denom) # First digit
-    counter <- 1
-    while(TRUE){
-        len <- length(cycle)
-        if(len %% 2 == 0){
-            subCycle <- cycle[1:(len/2)]
-            if(identical(cycle, rep(subCycle, 2))){
-                return(counter)
-            }
-            counter <- counter + 1
-        }
-        num <- num*10
-        remainder <- num %% denom
-        cycle <- c(cycle, (remainder * 10) %/% denom)
-    }
-    return(counter)
+getCycleLength <- function(num, denom){
+    quotients <- decimalDivision(num, denom)
+    cycleIndex <- which(quotients == tail(quotients, 1))
+    return(cycleIndex[2] - cycleIndex[1])
 }
 
 longestCycle <- function(limit){
     d <- d_max <- 3
     while(d < limit){
         d <- d + 1
-        print(d)
-        if(getCycleFraction(denom = d) > getCycleFraction(denom = d_max)){
+        if(getCycleLength(1, denom = d) > getCycleLength(1, denom = d_max)){
             d_max <- d
         }
     }
     return(d_max)
 }
 
-
-#################################
-findFirstDivisor <-function(num){
-    d <- 2
-    while(d < sqrt(num)){
-        if(num %% d == 0) return(d)
-        d <- d + 1
-    }
-    return("prime number")
-}
+longestCycle(1e3)
--- R/codeAnalysis.R
+++ R/codeAnalysis.R
@@ -1,4 +1,4 @@
 library(profvis)
 library(htmlwidgets)
-var <- profvis::profvis(fibonacciIndexLA(1e3))
+var <- profvis::profvis(longestCycle(1e3))
 saveWidget(var, "var.html")
