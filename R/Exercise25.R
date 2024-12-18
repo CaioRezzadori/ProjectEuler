36f17a8eac49890b9a1a738ee1995919e47a41c1
caio <caio.rezzadori@gmail.com>
2024-12-18 17:14:21 -0300

    Finalizing exercise 25

--- R/Exercise25.R
+++ R/Exercise25.R
@@ -23,47 +23,49 @@ fibonacciNumberIterative <- function(iter){
 
 # Iterative solution
 
-fibonacciIndex <- function(digitLimit){
+fibonacciIndex <- function(d){
     fib_1 <- fib_2 <- as.bigz(1)
     fib_3 <- fib_1 + fib_2
-    index <- 3
-    limit <- as.bigz(10)**(digitLimit - 1)
-    while(fib_3 %/% limit == 0){
+    n <- 3 # Iteration
+    limit <- as.bigz(10)**(d - 1)
+    while(fib_3 < limit){
         fib_1 <- fib_2
         fib_2 <- fib_3
         fib_3 <- fib_1 + fib_2
-        index <- index + 1
+        n <- n + 1
     }
-    return(index)
+    return(n)
 }
 
+# Library to calculate log10
 library(Rmpfr)
+
 # Using linear algebra
-fibonacciIndexLA <- function(digitLimit){ # digitLimit = d
-    # Sepctral decomposition of linear transformation
+fibonacciIndexLA <- function(d){ # digitLimit = d
+    # Spectral decomposition of linear transformation
     T <- matrix(c(0, 1, 1, 1), 2, 2, byrow = TRUE)
     ev <- eigen(T)
-    v_0 <- c(1, 1) # [f_0, f_1]
+    v_0 <- c(1, 1) # [f_1, f_2]
     V <- ev$vectors #Eigenvectors
     lambda <- ev$values #Eigenvalues
 
-    # Calculating initial value to iterations
-    limit <- as.bigz(10)**(digitLimit - 1)
-    n <- ceiling((digitLimit - 1 - log10(1.5))/log10(lambda[1]) + 1)
-    lambda <- as.bigq(lambda)
+    # Calculating n_0 (initial value to iterations)
+    limit <- as.bigz(10)**(d - 1)
+    n <- ceiling((d - 1 - log10(1.5))/log10(lambda[1]) + 2)
 
     # Power of n-1 of eigenvalue matrix
+    lambda <- as.bigq(lambda)
     L <- as.bigq(diag(c(1, 1)))
-    L[1] <- lambda[1]** (n - 1)
-    L[4] <- lambda[2]** (n - 1)
+    L[1] <- lambda[1]** (n - 2)
+    L[4] <- lambda[2]** (n - 2)
 
     # The inverse of V is equal to its transpose, since T is symmetric
     T <-  V %*% L %*% t(V)
     f <- T %*% v_0 # [f_(n-1), f_n]
 
-    browser()
     # Finding result
-    while(f[2]/limit < 1){
+    cat("Initial value:", n, "\n")
+    while(f[2] < limit){
         temp <- f[1]
         f[1] <- f[2]
         f[2] <- f[1] + temp
@@ -72,22 +74,23 @@ fibonacciIndexLA <- function(digitLimit){ # digitLimit = d
     return(n)
 }
 
-# source("R/Exercise25.R")
-fibonacciIndexLA(1e3)
+fibonacciIndexLA(1e4)
+
+fibonacciIndex(1e2)
 
 
-fibonacciNumber <- function(iter){
-    T <- matrix(c(0, 1, 1, 1), 2, 2, byrow = TRUE)
-    ev <- eigen(T)
-    vec <- c(1, 1)
-    V <- ev$vectors
+# fibonacciNumber <- function(n){
+#     T <- matrix(c(0, 1, 1, 1), 2, 2, byrow = TRUE)
+#     ev <- eigen(T)
+#     vec <- c(1, 1)
+#     V <- ev$vectors
 
-    ev$values <- as.bigq(ev$values)
-    L <- as.bigq(diag(c(1, 1)))
-    L[1] <- ev$values[1]** (iter - 1)
-    L[4] <- ev$values[2]** (iter - 1)
-    T <-  V %*% L %*% t(V)
-    values <- T %*% vec
+#     ev$values <- as.bigq(ev$values)
+#     L <- as.bigq(diag(c(1, 1)))
+#     L[1] <- ev$values[1]** (n - 1)
+#     L[4] <- ev$values[2]** (n - 1)
+#     T <-  V %*% L %*% t(V)
+#     values <- T %*% vec
 
-    return(as.numeric(values[1]))
-}
+#     return(as.numeric(values))
+# }
