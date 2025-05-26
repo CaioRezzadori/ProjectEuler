11d039a1dcd7d34b8aa5c0d2e3f81cee5d79f306
CaioRezzadori <caio.rezzadori@gmail.com>
2025-05-26 17:43:23 -0300

    Adjusting last exercises

--- python/minesWeeper.py
+++ python/minesWeeper.py
@@ -3,6 +3,61 @@ def minesWeeper(board):
     if(any([len(x) != boardLength for x in board])):
         return(-1) # Dimension error
 
+    mineCounter = 0
+    directions = [[0, -1], [0, 1], # Left, Right
+                  [1, 0], [-1, 0], # Down, Up
+                  [1, 1], [1, -1], # Diag down right, Diag down left
+                  [-1, 1], [-1, - 1]] # Diag up right, Diag up left
+    
+    for i in range(0, len(board)):
+        for j in range(0, boardLength):
+            for dir in directions:
+                if(board[i][j] == "*" or i + dir[0] < 0 or j + dir[1] < 0):
+                    continue
+                try:
+                    mineCounter = mineCounter + 1 if (board[i + dir[0]][j + dir[1]] == "*")\
+                                  else mineCounter
+                except IndexError:
+                    continue
+
+            if(mineCounter > 0):
+                board[i] = board[i][:j] + str(mineCounter) + board[i][j + 1:]
+                mineCounter = 0
+    return(board)
+
+board = ["·*·*·",
+         "··*··",
+         "··*··",
+         "·····"]
+
+assert minesWeeper(board) == ["1*3*1",
+                              "13*31",
+                              "·2*2·",
+                              "·111·"]
+
+minesWeeper(["·*·*·",
+             "··*··",
+             "··*··",
+             "··*··"]) == ["1*3*1",
+                           "13*31",
+                           "·3*3·",
+                           "·2*2·"]
+
+minesWeeper(["·*·*·",
+             "*·*··",
+             "··*··",
+             "··*··"]) == ["2*3*1",
+                           "*4*31",
+                           "14*3·",
+                           "·2*2·"]
+
+
+
+def minesWeeper(board):
+    boardLength = len(board[0])
+    if(any([len(x) != boardLength for x in board])):
+        return(-1) # Dimension error
+
     boardStr = "".join(board)
     mineCounter = 0
     directions = [-1, 1, # Left, Right
@@ -15,8 +70,7 @@ def minesWeeper(board):
             continue
         for dir in directions:
             try:
-                mineCounter = mineCounter + 1 if (boardStr[i + dir] == "*" and \
-                                                  i + dir >= 0)\
+                mineCounter = mineCounter + 1 if (boardStr[i + dir] == "*")\
                               else mineCounter
             except IndexError:
                 continue
@@ -26,21 +80,3 @@ def minesWeeper(board):
             mineCounter = 0
     return([boardStr[x:x + boardLength] for x in range(0, len(boardStr), boardLength)])
 
-
-board = ["·*·*·",
-         "··*··",
-         "··*··",
-         "·····"]
-
-minesWeeper(board) == ["1*3*1",
-                       "13*31",
-                       "·2*2·",
-                       "·111·"]
-
-minesWeeper(["·*·*·",
-             "··*··",
-             "··*··",
-             "··*··"]) == ["1*3*1",
-                           "13*31",
-                           "·3*3·",
-                           "·2*2·"]
--- python/tictactoeChecker.py
+++ python/tictactoeChecker.py
@@ -1,10 +1,22 @@
 # Check if row is filled
 def isRowWinner(field):
     fieldStr = ''.join(field)
-    if("XXX" in fieldStr or "OOO" in fieldStr):
-        return(True)
-    return(False)
+    return any([fieldStr[x: x + 3] in ["OOO", "XXX"] \
+           for x in range(0, len(fieldStr), 3)])
 
-isRowWinner(['X', 'X', 'X', 'O', 'O', '.', '.', '.', '.'])
-isRowWinner(['X', 'O', 'X', 'O', 'O', 'O', 'X', '.', 'X'])
-isRowWinner(['X', 'O', '.', '.', 'X', 'O', '.', '.', 'X'])
+isRowWinner(['X', 'X', 'X',
+             'O', 'O', '.',
+             '.', '.', '.'])
+isRowWinner(['X', 'O', 'X',
+             'O', 'O', 'O',
+             'X', '.', 'X'])
+isRowWinner(['X', 'O', '.',
+             '.', 'X', 'O',
+             '.', '.', 'X'])
+isRowWinner(['X', 'O', 'X',
+             'X', 'X', 'O',
+             '.', '.', 'O'])
+isRowWinner(['X', 'O', 'X',
+             'X', 'X', 'O',
+             'O', 'O', 'O'])
+
