958e5e88a4152b2a6247cfdd1d30bbb03f301d64
CaioRezzadori <caio.rezzadori@gmail.com>
2025-06-09 17:13:00 -0300

    Adjusting tests

--- src/projecteuler/saddlePoints.py
+++ src/projecteuler/saddlePoints.py
@@ -41,38 +41,3 @@ def saddlePoints(grid: list[list[int]]) -> list[tuple[int, int]]:
             solutions.append((row + 1, col + 1))
 
   return(solutions)
-
-
-saddlePoints([[9, 8, 7, 8],
-              [5, 3, 2, 4],
-              [6, 6, 7, 1]])
-
-
-saddlePoints([[10, 8, 7, 8],
-              [5, 3, 2,  5],
-              [11, 6, 1, 11]])
-
-saddlePoints([[9, 8, 7, 8],
-              [2, 3, 5, 4],
-              [6, 6, 7, 1]])
-
-saddlePoints([[9, 8, 7, 8], # 8 is max in row and max in col
-              [2, 3, 1, 4], # 4 is max in row and 1 < 4 (not min in col)
-              [6, 6, 7, 1]]) # 7 is max in row and 1 < 7 (not min in col)
-
-# Exception tests
-saddlePoints([[9, 8, 7, 8],
-              [2, 3, 1, 4],
-              [6, 6, -1, 1]])
-
-saddlePoints([[9, 8, '7', 8],
-              [2, 3, 1, 4],
-              [6, 6, 8, 1]])
-
-saddlePoints([[9, 8, 7, 8],
-              ['2', 3, 1, 4],
-              [6, 6, 8, 1]])
-
-saddlePoints([[9, 8, 7],
-              [2, 3, 5, 4],
-              [6, 6, 1]])
--- tests/python/test_saddlePoints.py
+++ tests/python/test_saddlePoints.py
@@ -1,2 +1,41 @@
+from projecteuler.saddlePoints import saddlePoints
+
 def test_example():
-  assert True
+  assert [(2, 1)] == saddlePoints([[9, 8, 7, 8],
+                                   [5, 3, 2, 4],
+                                   [6, 6, 7, 1]])
+
+
+# saddlePoints([[9, 8, 7, 8],
+#               [5, 3, 2, 4],
+#               [6, 6, 7, 1]])
+
+
+# saddlePoints([[10, 8, 7, 8],
+#               [5, 3, 2,  5],
+#               [11, 6, 1, 11]])
+
+# saddlePoints([[9, 8, 7, 8],
+#               [2, 3, 5, 4],
+#               [6, 6, 7, 1]])
+
+# saddlePoints([[9, 8, 7, 8], # 8 is max in row and max in col
+#               [2, 3, 1, 4], # 4 is max in row and 1 < 4 (not min in col)
+#               [6, 6, 7, 1]]) # 7 is max in row and 1 < 7 (not min in col)
+
+# # Exception tests
+# saddlePoints([[9, 8, 7, 8],
+#               [2, 3, 1, 4],
+#               [6, 6, -1, 1]])
+
+# saddlePoints([[9, 8, '7', 8],
+#               [2, 3, 1, 4],
+#               [6, 6, 8, 1]])
+
+# saddlePoints([[9, 8, 7, 8],
+#               ['2', 3, 1, 4],
+#               [6, 6, 8, 1]])
+
+# saddlePoints([[9, 8, 7],
+#               [2, 3, 5, 4],
+#               [6, 6, 1]])
