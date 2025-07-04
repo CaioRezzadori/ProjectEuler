b9a45f136fe84219d59ce259327e1e9722d6a44d
CaioRezzadori <caio.rezzadori@gmail.com>
2025-07-04 13:55:17 -0300

    Solving "rational_numbers" problem

--- src/projecteuler/rational_numbers.py
+++ src/projecteuler/rational_numbers.py
@@ -1,9 +1,15 @@
 class Rational:
-    def __init__(self, numerator: int | float, denominator: int | float):
+    """
+    Define rational number and it's operations
+    """
+
+    def __init__(self, numerator: int | float, denominator: int | float) -> None:
+        if denominator == 0:
+            raise ValueError("denominator must be different from 0")
         self.numerator = numerator
         self.denominator = denominator
 
-    def __add__(self, other: "Rational | int | float"):
+    def __add__(self, other: "Rational | int | float") -> "Rational":
         if isinstance(other, Rational):
             return Rational(
                 self.numerator * other.denominator + other.numerator * self.denominator,
@@ -11,10 +17,10 @@ class Rational:
             )
         return Rational(self.numerator + other * self.denominator, self.denominator)
 
-    def __radd__(self, other: int | float):
+    def __radd__(self, other: int | float) -> "Rational":
         return Rational(self.numerator + other * self.denominator, self.denominator)
 
-    def __sub__(self, other: "Rational | int | float"):
+    def __sub__(self, other: "Rational | int | float") -> "Rational":
         if isinstance(other, Rational):
             return Rational(
                 self.numerator * other.denominator - other.numerator * self.denominator,
@@ -22,15 +28,49 @@ class Rational:
             )
         return Rational(-self.numerator + other * self.denominator, self.denominator)
 
-    def __rsub__(self, other: int | float):
+    def __rsub__(self, other: int | float) -> "Rational":
         return Rational(-self.numerator + other * self.denominator, self.denominator)
 
-    def __mul__(self, other: "Rational | int | float"):
+    def __eq__(self, other: "Rational | float") -> bool:
+        return (self - other).numerator == 0
+
+    def __mul__(self, other: "Rational | int | float") -> "Rational":
         if isinstance(other, Rational):
             return Rational(
                 self.numerator * other.numerator, self.denominator * other.denominator
             )
         return Rational(other * self.numerator, self.denominator)
 
-    def __eq__(self, other: "Rational | float"):
-        return (self - other).numerator == 0
+    def __truediv__(self, other: "Rational | int | float") -> "Rational":
+        if isinstance(other, Rational):
+            return Rational(
+                self.numerator * other.denominator, self.denominator * other.numerator
+            )
+        return Rational(self.numerator, self.denominator * other)
+
+    def __rtruediv__(self, other: int | float) -> "Rational":
+        return Rational(other * self.denominator, self.numerator)
+
+    def __abs__(self) -> "Rational":
+        return Rational(abs(self.numerator), abs(self.denominator))
+
+    def __pow__(self, other: "Rational | int | float") -> "Rational":
+        if isinstance(other, Rational):
+            if other.numerator * other.denominator > 0:
+                return Rational(
+                    self.numerator ** (other.numerator / other.denominator),
+                    self.denominator ** (other.numerator / other.denominator),
+                )
+            elif self.numerator != 0 and other.numerator == 0:
+                return Rational(1, 1)
+            return Rational(
+                self.denominator ** (other.numerator / other.denominator),
+                self.numerator ** (other.numerator / other.denominator),
+            )
+        return Rational(self.numerator**other, self.denominator**other)
+
+    def __rpow__(self, other: int | float) -> float:
+        return other ** (self.numerator / self.denominator)
+
+    def __round__(self) -> "Rational":
+        return Rational(round(self.numerator, 8), round(self.denominator, 2))
