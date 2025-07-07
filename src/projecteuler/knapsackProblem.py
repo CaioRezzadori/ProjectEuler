014ecc076f3f790f6d01c0998789cc8e5d608134
CaioRezzadori <caio.rezzadori@gmail.com>
2025-07-07 13:47:04 -0300

    Solving knapsackProblem with combinatorics

--- src/projecteuler/knapsackProblem.py
+++ src/projecteuler/knapsackProblem.py
@@ -1,16 +1,29 @@
-def knapsack(W, values, weights):
-    n = len(values)
-    dp = [[0] * (W + 1) for _ in range(1, n + 1)]
+# def knapsackRec(W, param, n):
+def knapsackComb(W: int, param: list[dict[int, int]]):
+    combinations = [set()]
+    n = len(param)
+    maxValue = 0
     for i in range(n):
-        for j in range(W):
-            if i > 0 and j > 0:
-                pick = 0
-                if weights[i - 1] <= j:
-                    pick = values[i] + dp[i - 1][j - weights[i]]
-                notPick = dp[i - 1][j]
-                dp[i][j] = max(pick, notPick)
-    return dp[n - 1][W - 1]
+        for comb in combinations:
+            newComb = comb.union({i})
+            sumWeights, valSum = (0, 0)
+            for j in newComb:
+                sumWeights += param[j]['weight']
+                valSum += param[j]['value']
+            if sumWeights <= W and newComb not in combinations:
+                combinations.append(newComb)
+                if maxValue < valSum:
+                    maxValue = valSum
+    return maxValue
 
+def knapsackComb2(W: int, param: list[dict[int, int]]):
+    combinations = [[0, 0]]
+    for i in param:
+        combinations += [[comb[0] + i['weight'], comb[1] + i['value']] if \
+                          comb[0] + i['weight'] <= W else [0, 0] \
+                          for comb in combinations]
+    values = [val[1] for val in combinations]
+    return max(values)
 
-knapsack(W=10, values=[10, 40, 30, 50], weights=[5, 4, 6, 4])
-knapsack(W=4, values=[1, 2, 3], weights=[4, 5, 1])
+def maximum_value(W: int, param: list[dict[int, int]]):
+    return knapsackComb(W, param)
